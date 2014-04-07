0f338b96b1f4b8b77e3470ea71539f2bf9ab9caf
ops-class.org Student <trinity@zion.(none)>
2014-04-07 16:22:30 -0400

    asst2 changes

--- kern/arch/mips/locore/trap.c
+++ kern/arch/mips/locore/trap.c
@@ -108,13 +108,16 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 		break;
 	}
 
+	kprintf("Exiting thread %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
+		code, sig, trapcodenames[code], epc, vaddr);
+	
+	sys___exit(0);
+
 	/*
 	 * You will probably want to change this.
 	 */
 
-	kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
-		code, sig, trapcodenames[code], epc, vaddr);
-	panic("I don't know how to handle this\n");
+	//panic("I don't know how to handle this\n");
 }
 
 /*
