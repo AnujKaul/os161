ce42a15431f3a9cccb1dc51a84567994aa640ffc
ops-class.org Student <trinity@zion.(none)>
2014-05-07 02:37:27 -0400

    asst3

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -184,7 +184,7 @@ void write_page(unsigned int swpindx, paddr_t swap_page){
 	if(reserror){
 		panic("VM ERROR : Swapping out Failed");
 	}
-	tlb_invalidate_all();
+	tlb_invalidate(swap_page);
 }
 
 void tlb_invalidate(paddr_t paddr)
